a82bd22d8a6efbde700bc6b8b0baecdb94cf470a
Daiki Itoh <daikii@imgsrc.co.jp>
2015-02-11 16:26:37 +0900

    Add first
    Python OSC and simple crazyflie control.

---
+++ kinect-input.py
@@ -0,0 +1,160 @@
+"""
+
+Crazyflie controlled by Kinect via OSC from openFrameworks.
+
+"""
+
+import time, sys
+from threading import Thread
+
+#FIXME: Has to be launched from within the example folder
+sys.path.append("../lib")
+import cflib
+from cflib.crazyflie import Crazyflie
+
+import logging
+logging.basicConfig(level=logging.ERROR)
+
+import OSC
+
+#-------------------------------------------------------------------------
+
+class ReceiveOSC:
+
+    def __init__(self):
+        # tupple with ip, port
+        receive_address = 'localhost', 8000
+
+        # OSC Server
+        self.s = OSC.OSCServer(receive_address) # basic
+        ##s = OSC.ThreadingOSCServer(receive_address) # threading
+        ##s = OSC.ForkingOSCServer(receive_address) # forking
+
+        # this registers a 'default' handler (for unmatched messages)
+        self.s.addDefaultHandlers()
+
+    def receive(self):
+        #instantiate Input class
+        inp = Input(available[0][0])
+
+        # add a function call that gets data sent via OSC
+        self.s.addMsgHandler("/mouse/position", inp.drive_handler)
+
+        # check handlers added
+        print "\nRegistered Callback-functions are :"
+        for addr in self.s.getOSCAddressSpace():
+            print addr
+
+        # Start OSCServer
+        print "\nStarting OSCServer. Use ctrl-C to quit."
+        th = Thread( target = self.s.serve_forever )
+        th.start()
+
+        try :
+            while 1 :
+                time.sleep(0)
+
+        except KeyboardInterrupt :
+            # disconnect radio
+            inp._cf.close_link()
+
+            # close server
+            print "\nClosing OSCServer."
+            self.s.close()
+            
+            # close thread
+            print "Waiting for Server-thread to finish"
+            th.join()
+
+            print "Done"
+
+    # define a message-handler function for the server to call
+    def print_handler(self, addr, tags, stuff, source):
+        print "---"
+        print "received new osc msg from %s" % OSC.getUrlStr(source)
+        print "with addr : %s" % addr
+        print "typetags %s" % tags
+        print "data %s" % stuff
+
+#-------------------------------------------------------------------------
+
+class Input:
+
+    def __init__(self, link_uri):
+        self._cf = Crazyflie()
+
+        self._cf.connected.add_callback(self._connected)
+        self._cf.disconnected.add_callback(self._disconnected)
+        self._cf.connection_failed.add_callback(self._connection_failed)
+        self._cf.connection_lost.add_callback(self._connection_lost)
+
+        self._cf.open_link(link_uri)
+
+        print "Connecting to %s" % link_uri
+
+        # thrust value continuously modified
+        self.thrust = 10000
+
+    def _connected(self, link_uri):
+        """ This callback is called form the Crazyflie API when a Crazyflie
+        has been connected and the TOCs have been downloaded."""
+
+        # Start a separate thread to do the motor test.
+        # Do not hijack the calling thread!
+        #Thread(target=self._ramp_motors).start()
+
+    def _connection_failed(self, link_uri, msg):
+        """Callback when connection initial connection fails (i.e no Crazyflie
+        at the speficied address)"""
+        print "Connection to %s failed: %s" % (link_uri, msg)
+
+    def _connection_lost(self, link_uri, msg):
+        """Callback when disconnected after a connection has been made (i.e
+        Crazyflie moves out of range)"""
+        print "Connection to %s lost: %s" % (link_uri, msg)
+
+    def _disconnected(self, link_uri):
+        """Callback when the Crazyflie is disconnected (called in all cases)"""
+        print "Disconnected from %s" % link_uri
+
+    # handler called by OSC server
+    def drive_handler(self, addr, tags, stuff, source):
+        # increase thrust
+        if (stuff[0] == 1):
+            self.thrust += 500
+            pitch = 0
+            roll = 0
+            yawrate = 0
+            self._cf.commander.send_setpoint(roll, pitch, yawrate, self.thrust)
+            print self.thrust
+
+        # thrust reset
+        elif (stuff[0] == 2):
+            self.thrust = 10000
+            pitch = 0
+            roll = 0
+            yawrate = 0
+            self._cf.commander.send_setpoint(roll, pitch, yawrate, self.thrust)
+
+#-------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # Initialize the low-level drivers (don't list the debug drivers)
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+
+    # Scan for Crazyflies and use the first one found
+    print "\nScanning interfaces for Crazyflies..."    
+    available = cflib.crtp.scan_interfaces()
+        
+    # list available connection
+    print "Crazyflies found:"
+    for i in available:
+        print i[0]
+
+    # check radio connection
+    if len(available) > 0:
+        osc = ReceiveOSC()
+        osc.receive()
+    else:
+        print "No Crazyflies found, cannot run example"
