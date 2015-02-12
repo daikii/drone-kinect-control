9678fc2498be625884c414c4bc54aac7655dca0c
Daiki Itoh <daikii@imgsrc.co.jp>
2015-02-12 10:50:47 +0900

    Add oF interface
    Add oF interface

---
+++ droneKinectControl/src/ofApp.cpp
@@ -0,0 +1,154 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+
+void ofApp::setup(){
+    
+    // background
+    ofBackground(0,0,0);
+    
+    // connect to server
+    sender.setup(HOST, PORT);
+    
+    // enable depth->video image calibration
+    kinect.setRegistration(true);
+    
+    // init showing RGB
+    kinect.init();
+    
+    // opens first available kinect
+    kinect.open();
+    
+    kinectW = kinect.width;
+    kinectH = kinect.height;
+    
+    // reserve memory for cv images
+    colorImg.allocate(kinectW, kinectH);
+    hsbImg.allocate(kinectW, kinectH);
+    hueImg.allocate(kinectW, kinectH);
+    satImg.allocate(kinectW, kinectH);
+    briImg.allocate(kinectW, kinectH);
+    filtImg.allocate(kinectW, kinectH);
+}
+
+//--------------------------------------------------------------
+
+void ofApp::update(){
+    
+    //---------------------------------
+    // KINECT - COLOR TRACKING
+    //---------------------------------
+    
+    kinect.update();
+    
+    if(kinect.isFrameNew()){
+        // copy kinect pixels to rgb image
+        colorImg.setFromPixels(kinect.getPixels(), kinectW, kinectH);
+        
+        // mirror horizontally
+        colorImg.mirror(false, true);
+        
+        // duplicate rgb
+        hsbImg = colorImg;
+        
+        // convert to hsb
+        hsbImg.convertRgbToHsv();
+        
+        // store the three channels as grayscale images
+        hsbImg.convertToGrayscalePlanarImages(hueImg, satImg, briImg);
+        
+        // filter image based on the hue/sat value were looking for
+        for(int i = 0; i < kinectW * kinectH; i++) {
+            filtImg.getPixels()[i] = ofInRange(satImg.getPixels()[i], trackSat - 5, trackSat + 5) ? 255 : 0;
+        }
+        filtImg.flagImageChanged();
+        
+        // run the contour finder on the filtered image to find blobs with a certain hue/sat
+        contours.findContours(filtImg, 50, kinectW * kinectH / 2, 1, false);
+        
+        // store centroid of the tracked object
+        for(int i = 0; i < contours.nBlobs; i++){
+            trackPt.set(contours.blobs[i].centroid.x, contours.blobs[i].centroid.y);
+        }
+    }
+    
+    //---------------------------------
+    // OSC - POSITION UPDATE
+    //---------------------------------
+    
+    if(destSet && trackSet && launchSet){
+        // OSC prep
+        ofxOscMessage m;
+    
+        // OSC address select
+        m.setAddress("/drone/position");
+    
+        // compute distance x,y from tracked pt to destination
+        dist = destPt - trackPt;
+        ofLog() << dist;
+        
+        // add data arguments
+        m.addIntArg(1);
+    
+        // send via OSC
+        //sender.sendMessage(m);
+    }
+}
+
+//--------------------------------------------------------------
+
+void ofApp::draw(){
+
+    ofSetColor(255, 255, 255);
+    
+    // draw RGB image
+    colorImg.draw(0, 0);
+    
+    // draw destination point
+    if(destSet){
+        ofSetColor(255, 0, 0);
+        ofFill();
+        ofCircle(destPt, 10);
+    }
+    
+    // mark blob once hue/sat is chosen
+    if(trackSet){
+        // blob color
+        ofSetColor(0, 255, 0);
+        ofSetLineWidth(10);
+        ofNoFill();
+        
+        // draw circle for found blobs
+        for(int i = 0; i < contours.nBlobs; i++){
+            ofCircle(contours.blobs[i].centroid.x, contours.blobs[i].centroid.y,
+                     contours.blobs[i].boundingRect.width / 2);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+
+void ofApp::mousePressed(int x, int y, int button){
+    
+    // get hue/sat value on mouse position
+    trackSat = satImg.getPixels()[y * kinectW + x];
+    ofLog() << "tracked object sat: " << trackSat;
+    
+    trackSet = true;
+}
+
+//--------------------------------------------------------------
+
+void ofApp::keyPressed(int key){
+    
+    // set destination point at mouse location
+    if(key == 'w'){
+        destPt.set(mouseX, mouseY);
+        destSet = true;
+    }
+    
+    // trigger drone flight
+    if(key == 's'){
+        launchSet = true;
+    }
+}
---
+++ droneKinectControl/src/ofApp.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+#include "ofxOsc.h"
+
+#define HOST "localhost"
+#define PORT 8000
+
+class ofApp : public ofBaseApp{
+    
+    public:
+        void setup();
+        void update();
+        void draw();
+        void mousePressed(int x, int y, int button);
+        void keyPressed(int key);
+    
+        ofxKinect kinect;
+    
+        ofxCvColorImage colorImg;
+        ofxCvColorImage hsbImg;
+    
+        ofxCvGrayscaleImage hueImg;
+        ofxCvGrayscaleImage satImg;
+        ofxCvGrayscaleImage briImg;
+        ofxCvGrayscaleImage filtImg;
+    
+        ofxCvContourFinder contours;
+    
+        int kinectW;
+        int kinectH;
+    
+        int trackSat;
+    
+        ofVec2f destPt;
+        ofVec2f trackPt;
+        ofVec2f dist;
+    
+        bool trackSet = false;
+        bool destSet = false;
+        bool launchSet = false;
+    
+    private:
+        ofxOscSender sender;
+};
